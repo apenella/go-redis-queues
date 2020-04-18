bfd1e9e532936221405081c72f22ad0438a2a378
Aleix Penella <aleix.penella@edreamsodigeo.com>
2020-04-18 16:29:02 +0200

    include new flags for configuration

--- internal/application/command/go-redis-queues/go-redis-queues.go
+++ internal/application/command/go-redis-queues/go-redis-queues.go
@@ -12,7 +12,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type redisPubSubCmdFlags struct {
+	RedisHost     string
+	RedisPort     uint16
+	RedisDB       uint8
+	RedisPassword string
+}
+
+var redisPubSubCmdFlagsVars *redisPubSubCmdFlags
+
 func NewCommand(ctx context.Context, config *configuration.Configuration) *command.AppCommand {
+
+	redisPubSubCmdFlagsVars = &redisPubSubCmdFlags{}
 
 	if ctx == nil {
 		ctx = context.TODO()
@@ -29,6 +40,23 @@ go-redis-queues interactuates with Redis and its types which could be used as qu
  - Stream
 `,
 		Run: redisPubSubHandler,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if redisPubSubCmdFlagsVars.RedisPort != configuration.RedisPortDefaultValue {
+				config.RedisPort = redisPubSubCmdFlagsVars.RedisPort
+			}
+
+			if redisPubSubCmdFlagsVars.RedisHost != configuration.RedisHostDefaultValue {
+				config.RedisHost = redisPubSubCmdFlagsVars.RedisHost
+			}
+
+			if redisPubSubCmdFlagsVars.RedisDB != configuration.RedisDBDefaultValue {
+				config.RedisDB = redisPubSubCmdFlagsVars.RedisDB
+			}
+
+			if redisPubSubCmdFlagsVars.RedisPassword != configuration.RedisPasswordDefaultValue {
+				config.RedisPassword = redisPubSubCmdFlagsVars.RedisPassword
+			}
+		},
 	}
 
 	appCommand := &command.AppCommand{
@@ -37,9 +65,14 @@ go-redis-queues interactuates with Redis and its types which could be used as qu
 		Ctx:           ctx,
 	}
 
+	redisPubSubCmd.PersistentFlags().StringVarP(&redisPubSubCmdFlagsVars.RedisHost, "host", "H", configuration.RedisHostDefaultValue, "Redis host")
+	redisPubSubCmd.PersistentFlags().Uint16VarP(&redisPubSubCmdFlagsVars.RedisPort, "port", "P", configuration.RedisPortDefaultValue, "Redis port")
+	redisPubSubCmd.PersistentFlags().Uint8VarP(&redisPubSubCmdFlagsVars.RedisDB, "database", "D", configuration.RedisDBDefaultValue, "Redis database")
+	redisPubSubCmd.PersistentFlags().StringVarP(&redisPubSubCmdFlagsVars.RedisPassword, "password", "X", configuration.RedisPasswordDefaultValue, "Redis password")
+
 	appCommand.AddCommand(middleware.New(middleware.PanicRecover).Then(middleware.ErrorManagement(consume.NewCommand(ctx, config))))
 	appCommand.AddCommand(middleware.New(middleware.PanicRecover).Then(get.NewCommand(ctx, config)))
-	appCommand.AddCommand(middleware.New(middleware.PanicRecover).Then(publish.NewCommand(ctx, config)))
+	appCommand.AddCommand(middleware.New(middleware.PanicRecover).Then(middleware.ErrorManagement(publish.NewCommand(ctx, config))))
 
 	return appCommand
 }
